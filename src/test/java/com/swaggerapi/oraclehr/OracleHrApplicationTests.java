1e780c4f8940857c96f015ddaeb38942e96e8b31
Daniel <daniel.bennin@fasylgh.com>
2019-02-05 02:49:53 +0000

    Project set up

---
+++ src/test/java/com/swaggerapi/oraclehr/OracleHrApplicationTests.java
@@ -0,0 +1,17 @@
+package com.swaggerapi.oraclehr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OracleHrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
